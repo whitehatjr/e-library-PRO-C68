dcc601a11cfd39ec30f195277761459d0086d816
vishalgaddam873 <vishal18@navgurukul.org>
2021-04-12 14:53:28 +0530

    Initial Comment

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return <BottomTabNavigator />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Transaction" component={TransactionScreen} />
+          <Tab.Screen name="Search" component={SearchScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
